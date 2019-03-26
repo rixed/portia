02477defd42b1391198d4a6001c07193fb2d15f5
Cedric Cellier <rixed@happyleptic.org>
2019-03-26 15:14:57 +0100

    Build: Nix packaging

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ stdenv, fetchFromGitHub, ocaml, findlib, batteries }:
+
+stdenv.mkDerivation rec {
+  pname = "portia";
+  version = "1.2";
+
+  src = fetchFromGitHub {
+    owner = "rixed";
+    repo = "portia";
+    rev = "v${version}";
+    sha256 = "18c430gxfvwbafpay42p0clmb4cnxqdn0g1vadydlf67sxf1lkr0";
+  };
+
+  buildInputs = [ ocaml findlib batteries ];
+
+  createFindlibDestdir = true;
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/rixed/portia;
+    description = "Literate Programming Preprocessor";
+    platforms = ocaml.meta.platforms or [];
+    maintainers = [ maintainers.rixed ];
+  };
+}
