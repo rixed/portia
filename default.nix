42ee87e28bcd1f3523f9cdc75bce216970df22eb
Cedric Cellier <rixed@happyleptic.org>
2019-11-17 17:01:44 +0100

    Build: update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, ocaml, findlib, batteries }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (rec {
   pname = "portia";
   version = "1.3";
 
@@ -8,12 +8,19 @@ stdenv.mkDerivation rec {
     owner = "rixed";
     repo = "portia";
     rev = "v${version}";
-    sha256 = "18c430gxfvwbafpay42p0clmb4cnxqdn0g1vadydlf67sxf1lkr0";
+    sha256 = "1jq77rfl4v6b2v7fqv6vda0diqg5xxz4aiga4djdriwf1l086yy4";
   };
 
   buildInputs = [ ocaml findlib batteries ];
 
   createFindlibDestdir = true;
+
+  postInstall = ''
+    mkdir -p $out/bin
+    cp portia $out/bin
+  '';
+
+  dontStrip = !ocaml.nativeCompilers;
 
   meta = with stdenv.lib; {
     homepage = https://github.com/rixed/portia;
@@ -21,4 +28,4 @@ stdenv.mkDerivation rec {
     platforms = ocaml.meta.platforms or [];
     maintainers = [ maintainers.rixed ];
   };
-}
+})
