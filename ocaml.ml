91bd596ec793bb5e041783b7cfa4d7b42c953543
Cedric Cellier <rixed@happyleptic.org>
2021-09-21 18:43:14 +0200

    Build + cosmetics

--- ocaml.ml
+++ ocaml.ml
@@ -1,6 +1,6 @@
 
 # 288 "definitions.fw"
 let linenum lineno fname =
-    Printf.sprintf "# %d %S" (lineno+1) fname
+    Printf.sprintf "# %d \"%s\"" (lineno+1) fname
 
 let () = PortiaConfig.linenum := linenum
